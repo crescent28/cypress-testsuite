46f3d758f6b8f68bc15d2de7ba4c250377f38c2e
crescent28 <41110073+crescent28@users.noreply.github.com>
2019-10-30 12:15:26 +0530

    Create product_bundle.js

---
+++ product_bundle.js
@@ -0,0 +1,14 @@
+context('Table MultiSelect', () => {	
+	beforeEach(() => {
+	cy.login();
+	});
+	
+	it('Generate Product Bundle’, () => {
+	cy.new_form(“Product Bundle”);
+	cy.fill_field("new_item_code", “123", "Link");
+	// cy.fill_field(“transaction_date”, "16-10-2019", "Date");
+	cy.fill_table("items", [{"item_code": ""} ,{"qty": "2"}, {“description": “1019"}]);
+	cy.save();
+	cy.submit();
+	});
+	});
