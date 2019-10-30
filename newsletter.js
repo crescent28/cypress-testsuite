0d2375468d36e29ad89568f668f23a7e7e33d7bd
crescent28 <41110073+crescent28@users.noreply.github.com>
2019-10-30 13:59:18 +0530

    Create newsletter.js

---
+++ newsletter.js
@@ -0,0 +1,17 @@
+context('Table MultiSelect', () => {	
+	beforeEach(() => {
+	cy.login();
+	});
+	
+	it(‘Newsletter’, () => {
+	cy.new_form(“Newsletter”);
+	// cy.fill_field(“new_item_code”, “123", "Link");
+	
+	cy.fill_table(“email_group", [{“email_group": "Test"} , {“total_subscribers": “2”}]);
+	cy.fill_field(“send_from”, "Michelle Conner <mitali@iwebnotes.com>", "Date");
+	cy.fill_field(“subject”, “Test", “Data");
+	cy.fill_field(“message”, “Test", “Data");
+	cy.save();
+	cy.submit();
+	});
+	});
