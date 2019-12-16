eadaad484d0b454cd8e5e8c23862483bd09b8e32
crescent28 <41110073+crescent28@users.noreply.github.com>
2019-12-16 13:25:59 +0530

    Update sales_invoice.js

--- sales_invoice.js
+++ sales_invoice.js
@@ -1,1 +1,18 @@
+// Test case for creating a simple Sales Invoice.
+
+context('Table MultiSelect', () => {
+	beforeEach(() => {
+		cy.login();
+	});
+
+	it('Generate Sales Invoice', () => {
+		cy.new_form("Sales Invoice");
+		cy.fill_field("customer", "Burosys", "Link");
+   		cy.fill_field("transaction_date", "16-10-2019", "Date");
+  		cy.fill_table("items", [{"item_code": "1019"} , {"quantity": "2"}, {rate: "3,345,345.0000"}, {"amount": "6,690,690.0000"},]);
+		cy.save();
+				cy.submit();
+    		cy.get_button("Ledger");
+	});
+});
 
