bb59ccfb0c638a0d86847be6069be7cd15ce97a6
crescent28 <41110073+crescent28@users.noreply.github.com>
2019-12-16 15:32:01 +0530

    Create work-order.js

---
+++ integration/work-order.js
@@ -0,0 +1,20 @@
+// Test case for creating a simple Work Order
+
+context('Table MultiSelect', () => {
+	beforeEach(() => {
+		cy.login();
+	});
+
+	it('Generate Work Order', () => {
+		cy.new_form("Work Order");
+		cy.fill_field("production_item", "1019", "Link");
+    cy.fill_field("wip_warehouse", "All Warehouses - CIIT", "Link");
+   	 cy.fill_field("qty", "1", "Float");
+     cy.fill_field("planned_start_date","24-12-2019 15:28:24", Datetime);
+  	//	cy.fill_table("required_items", [{"item_code": "1019"} ,{"source_warehouse": "16-10-2019"}, {"quantity": "2"}, {rate: "3,345,345.0000"}]);
+				cy.save();
+		   cy.submit();
+    		cy.get_button("Ledger");
+	});
+});
+© 2019 GitHub, Inc.
